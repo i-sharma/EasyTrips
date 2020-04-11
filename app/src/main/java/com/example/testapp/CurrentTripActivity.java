c85024a773b8d2649bb197e85e4d09c208b7d939
Eeshan Deo <eeshandeo45@gmail.com>
2020-04-11 20:50:09 +0530

    delete button in card view

--- app/src/main/java/com/example/testapp/CurrentTripActivity.java
+++ app/src/main/java/com/example/testapp/CurrentTripActivity.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -38,6 +40,7 @@ public class CurrentTripActivity extends AppCompatActivity {
 
 
     private static final String TAG = "MainActivity";
+    private ArrayList<Integer> trip_indices;
     ViewPager viewPager;
     CurrentTripAdapter adapter;
     List<CurrentTripModel> models = new ArrayList<>();
@@ -45,6 +48,7 @@ public class CurrentTripActivity extends AppCompatActivity {
     ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 //    private int[] ids = new int[]{12,1,13,2,4,5,6};
     Button route;
+    LinearLayout removeItem;
 
     BottomNavigationView navigation;
     ArrayList<Integer> ids;
@@ -56,10 +60,26 @@ public class CurrentTripActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.activity_current_trip);
 
+        Integer[] colors_temp = {
+                getResources().getColor(R.color.color7),
+                getResources().getColor(R.color.color9),
+                getResources().getColor(R.color.color10),
+                getResources().getColor(R.color.color11),
+                getResources().getColor(R.color.color12),
+                getResources().getColor(R.color.color13),
+                getResources().getColor(R.color.color14),
+                getResources().getColor(R.color.color15),
+                getResources().getColor(R.color.color16)
+        };
+
+        colors = colors_temp;
+
 //        ids = (ArrayList<Integer>) getIntent().getSerializableExtra("trip_indices");
-        loadData();
+        loadTripData();
 
         route = findViewById(R.id.showRoute);
+        viewPager = findViewById(R.id.viewPager);
+        removeItem = findViewById(R.id.removeItemFromTrip);
 
         createStorageReference("delhi");
 
@@ -73,9 +93,26 @@ public class CurrentTripActivity extends AppCompatActivity {
             }
         });
 
+        removeItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeFromModel();
+                saveTripData();
+            }
+        });
+
     }
 
-    private void loadData(){
+    private void saveTripData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(trip_indices);
+        editor.putString("trip_indices", json);
+        editor.apply();
+    }
+
+    private void loadTripData(){
         SharedPreferences sharedPreferences = getSharedPreferences("shared_preferences", MODE_PRIVATE);
         Gson gson = new Gson();
         String json = sharedPreferences.getString("trip_indices", null);
@@ -122,6 +159,44 @@ public class CurrentTripActivity extends AppCompatActivity {
         return tourist_places;
     }
 
+    private void setViewPagerBackground() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                if(adapter != null){
+
+                    if (position < (adapter.getCount() -1) && position < (colors.length - 1)) {
+                        viewPager.setBackgroundColor(
+
+                                (Integer) argbEvaluator.evaluate(
+                                        positionOffset,
+                                        colors[position],
+                                        colors[position + 1]
+                                )
+                        );
+                    }
+
+                    else {
+                        viewPager.setBackgroundColor(colors[colors.length - 1]);
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+
+        });
+    }
+
     private void createStorageReference(String... cities) {
 
         HashMap<Integer, DocumentReference> tp = CreateDocReference(cities);
@@ -164,62 +239,28 @@ public class CurrentTripActivity extends AppCompatActivity {
 
     }
 
+    public void removeFromModel() {
+        Log.d("deleting:",""+viewPager.getCurrentItem());
+        int position = viewPager.getCurrentItem();
+
+        models.remove(position);
+
+        adapter = new CurrentTripAdapter(models, this);
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(position);
+        viewPager.setPadding(100, 0, 100, 0);
+        setViewPagerBackground();
+    }
+
     private void addToModel(Uri result, String title, String short_description, int id) {
         models.add(new CurrentTripModel(result,title,short_description));
         Log.d("model contains ",""+id);
-        /*if(id == ids[ids.length - 1])    createAdapter();*/
+
         adapter = new CurrentTripAdapter(models, this);
-        viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(adapter);
         viewPager.setPadding(130, 0, 130, 0);
 
-        Integer[] colors_temp = {
-                getResources().getColor(R.color.color5),
-                getResources().getColor(R.color.color7),
-                getResources().getColor(R.color.color9),
-                getResources().getColor(R.color.color10),
-                getResources().getColor(R.color.color11),
-                getResources().getColor(R.color.color12),
-                getResources().getColor(R.color.color13),
-                getResources().getColor(R.color.color14),
-                getResources().getColor(R.color.color15),
-                getResources().getColor(R.color.color16)
-        };
-
-        colors = colors_temp;
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-                if (position < (adapter.getCount() -1) && position < (colors.length - 1)) {
-                    viewPager.setBackgroundColor(
-
-                            (Integer) argbEvaluator.evaluate(
-                                    positionOffset,
-                                    colors[position],
-                                    colors[position + 1]
-                            )
-                    );
-                }
-
-                else {
-                    viewPager.setBackgroundColor(colors[colors.length - 1]);
-                }
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-
-        });
-
+        setViewPagerBackground();
     }
 
 }
